2ec1380f6189cdb4a9681817fd6432bea45f3e3c
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-05-01 19:57:39 +0800

    update

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,14 +1,31 @@
 import { Link } from "react-router";
 import type { Route } from "./+types/home";
+import { useEffect, useState } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Movie app" }];
 }
 
 export default function Home() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log(counter);
+  }, [counter]);
+
+  console.log("--");
+
   return (
     <div>
       <h1>Movie </h1>
+
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        button
+      </button>
 
       <Link to="movies">Movies</Link>
       <Link to="aothors">Authors</Link>
